7bda27744391554b2ebb6f1dcda9b291ba50e29a
c3125563 <c3125563@uon.edu.au>
2018-12-03 19:47:34 +1100

    Included hello message

--- day01/solution.py
+++ day01/solution.py
@@ -1,6 +1,7 @@
-freq=[]
 with open("input.txt") as f:
   data = f.read().splitlines()
   
 if __name__ == "__main__":
   print("Solution to problem 1 is {}".format(sum([int(i) for i in data])))
+
+print("hello")
