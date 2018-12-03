fa716fa0e40168cdd21e8cd14ed140af26531bf4
c3125563 <c3125563@uon.edu.au>
2018-12-03 21:43:23 +1100

    Included solution to problem 2

--- day01/solution.py
+++ day01/solution.py
@@ -1,5 +1,22 @@
 with open("input.txt") as f:
   data = f.read().splitlines()
-  
+
+freq = [int(i) for i in data]
+
+ 
+seen = set()
+val = 0
+cont = True
+
+while cont:
+  for i in freq:
+    val += i
+    if val in seen:
+      print("duplicate found:", val)
+      cont = False
+      break
+    seen.add(val)
+
 if __name__ == "__main__":
-  print("Solution to problem 1 is {}".format(sum([int(i) for i in data])))
+  print("Solution to problem 1 is {}".format(sum(freq)))
+  print("Solution to problem 2 is {}".format(val))
