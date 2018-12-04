cd9183da4885b51b2b1ea5f3dc52e283314c192c
pgoldthorpe <43900257+pgoldthorpe@users.noreply.github.com>
2018-12-04 14:01:05 +1100

    Update solution02.py

--- day02/solution02.py
+++ day02/solution02.py
@@ -3,43 +3,16 @@ with open(".\day02\input.txt") as f:
   data = f.read().splitlines()
 
 def duplicate(iterable):
-    """
-    Returns a boolean tuple if the iterable contains two and three of the same element, respectively.
-
-    Parameters
-    ----------
-    iterable : interable
-            The iterable to be iterated over.
-
-    Returns
-    -------
-    tuple : tuple
-            A tuple containing the boolean response to the string having doublet or triplet of element.
-    
-    Examples
-    --------
-    >>> 'abc'
-    (False, False)
-
-    >>> 'aabb'
-    (True, False)
-
-    >>> 'aaabb'
-    (True, True)
-
-    >>> 'aaab'
-    (False, True)
-    """
     count = dict()
     for i in iterable:
         count[i] = count.get(i, 0) + 1
-    return (2 in count.values(), 3 in count.values())
+    return count
 
-twos, threes = 0, 0
+doublet, triplet = 0, 0
 for line in data:
-    twos += duplicate(line)[0]
-    threes += duplicate(line)[1]
+    doublet += (2 in duplicate(line).values())
+    triplet += (3 in duplicate(line).values())
 
 if __name__ == "__main__":
-  print("Solution to problem 1 is {}".format(twos*threes))
+  print("Solution to problem 1 is {}".format(doublet * triplet))
   print("Solution to problem 2 is {}".format('hello'))
