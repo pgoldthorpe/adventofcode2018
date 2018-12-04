352ef8e4f143429a3eede5ba21fdb8565c47dcf5
c3125563 <c3125563@uon.edu.au>
2018-12-04 23:13:35 +1100

    Completed part 2

--- day03/solution03.py
+++ day03/solution03.py
@@ -13,6 +13,13 @@ def sheet(matrix, x, y, w, h):
             matrix[i][j]+=1
     return matrix
 
+def single_sheet(matrix, x, y, w, h):
+    number = 0
+    for i in range(y, y+h):
+        for j in range(x, x+w):
+            number += matrix[i][j]
+    return number
+
 fabric = create_matrix(1000,1000)
 for line in data:
   _, _, x, y, _, w, h = re.split('x|,|:| ', line)
@@ -22,6 +29,11 @@ count = 0
 for i in range(1000):
     count += sum(i>1 for i in fabric[i])
 
+for line in data:
+  _, id, _, x, y, _, w, h = re.split('x|,|:| |#', line)
+  if single_sheet(fabric, int(x), int(y), int(w), int(h)) == int(w)*int(h):
+      break
+
 if __name__ == "__main__":
   print("Solution to problem 1 is {}".format(count))
-  print("Solution to problem 2 is {}".format('hello'))
+  print("Solution to problem 2 is {}".format(id))
