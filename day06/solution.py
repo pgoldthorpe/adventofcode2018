3bb09bb25b8f6f39f79870d8f44fc9cd79106ca7
Peter <petergoldthorpe@gmail.com>
2018-12-08 12:39:31 +1100

    Update

--- day06/solution.py
+++ day06/solution.py
@@ -1,2 +1,7 @@
 with open(".\day06\input.txt") as f:
   data = f.read().splitlines()
+
+
+if __name__ == "__main__":
+  print("Solution to problem 1 is {}".format('Hello'))
+  print("Solution to problem 2 is {}".format("Hello"))
